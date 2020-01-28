eea8840b0ab21aa0f62815cee2423cd3de09e8cf
osaulo <osaulo_tarso@hotmail.com>
2020-01-28 02:40:47 -0300

    ajuste heroku

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
+const port = process.env.PORT || 3000
  
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
  
@@ -9,4 +10,4 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
  
-app.listen(process.env.PORT || 8080);
+app.listen(port);
