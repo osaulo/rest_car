b30b1802d944b85bf70d628c8c9aafb3bb67424e
osaulo <osaulo_tarso@hotmail.com>
2020-01-28 02:55:12 -0300

    ajuste heroku

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,18 @@
+//Importa as dependências que acabamos de instalar
 const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
+const port = process.env.PORT || 8081
+
 const app = express();
-const port = process.env.PORT || 3000
- 
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`'));
 });
- 
+
+// Inicia a aplicação pela porta configurada
 app.listen(port);
